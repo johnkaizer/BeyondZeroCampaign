39577e9de1b98db48bc9bfdc7a9ce5a8d2a19f12
john kaiser <52598487+johnkaizer@users.noreply.github.com>
2022-10-10 21:43:39 +0300

    Appointment ui

---
+++ app/src/main/java/com/project/beyondzero/DBmain.java
@@ -0,0 +1,29 @@
+package com.project.beyondzero;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+public class DBmain extends SQLiteOpenHelper {
+    public static final String DBNAME = "beyond.db";
+    public static final String TABLENAME = "appointment";
+    public static final int VER = 1;
+    public DBmain(@Nullable Context context) {
+        super(context, DBNAME, null, VER);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        String query = "create table "+TABLENAME+" (id integer primary key, avatar blob, name text, title text,patient text, time text,phone integer)";
+        db.execSQL(query);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        String query ="drop table if exists "+TABLENAME+" ";
+        db.execSQL(query);
+
+    }
+}
